48a2e0ef909cd4c8782197d596e499af49d4b281
JameSmith-1998 <83923013+JameSmith-1998@users.noreply.github.com>
2021-05-28 01:35:03 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -7,11 +7,11 @@ from pyrogram import filters, Client, emoji
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 
-@StreamBot.on_message(filters.private & (filters.document | filters.video | filters.audio), group=4)
+@StreamBot.on_message(filters.private & (filters.document | filters.video | filers.photo | filters.audio), group=4)
 async def media_receive_handler(c: Client, m: Message):
     log_msg = await m.copy(chat_id=Var.BIN_CHANNEL)
-    stream_link = "https://{}/{}".format(Var.FQDN, log_msg.message_id) if Var.ON_HEROKU or Var.NO_PORT else \
-        "http://{}:{}/{}".format(Var.FQDN,
+    stream_link = "https://{}/stream/{}".format(Var.FQDN, log_msg.message_id) if Var.ON_HEROKU or Var.NO_PORT else \
+        "http://{}:{}/stream/{}".format(Var.FQDN,
                                 Var.PORT,
                                 log_msg.message_id)
     await m.reply_text(
